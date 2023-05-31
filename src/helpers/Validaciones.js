d4b96dc11ecb8055d41e68c034b913b5e911a670
Diego Mercado <diego.mercado1997@hotmail.com>
2023-05-31 11:24:48 -0300

    correcion de errores

--- src/helpers/Validaciones.js
+++
@@ -1,8 +0,0 @@
-export  default function validacionAnio(anio){
-    // 1895 - (año actual + 1)
-    const fecha = (new Date().getFullYear()) + 1;
-    if(anio > 2022 && anio <= fecha){
-        return true;
-    }else{
-        return false;
-    }}
