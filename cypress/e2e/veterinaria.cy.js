747c44a2ef1bb8bf931c5540c1d587187ba9a713
Diego Mercado <diego.mercado1997@hotmail.com>
2023-11-02 19:34:42 -0300

    realizo test con cypress

--- cypress/e2e/veterinaria.cy.js
+++ cypress/e2e/veterinaria.cy.js
@@ -1,6 +1,16 @@
 describe("Formulario Veterinaria", () => {
+    const NOMBRE_MASCOTA = "Homero";
+    const NOMBRE_DUENIO = "Diego";
+    const HORA_RESERVA_TURNO = "10:00";
+    const FECHA_TURNO = "2023-11-02";
+    const SINTOMAS_MASCOTA = "Sintomas de la mascota";
+
     beforeEach(() => {
         cy.visit("http://localhost:5173");
+    });
+
+    it("Deberia existir el formulario", () => {
+        cy.get("Form").should("exist");
     });
 
     it("Deberia mostrar los mensajes de error si se envia el formulario vacio", () => {
@@ -15,12 +25,13 @@ describe("Formulario Veterinaria", () => {
     });
 
     it("Deberia agregar una nueva cita al enviar el formulario", () => {
-        cy.get("input[name='mascota']").type("Perro");
-        cy.get("input[name='duenio']").type("Dueño");
-        cy.get("input[name='fecha']").type("2023-06-15");
-        cy.get("input[name='hora']").type("09:00");
-        cy.get("textarea[name='sintomas']").type("sintomas de la mascota");
+        cy.get("input[name='mascota']").type(NOMBRE_MASCOTA);
+        cy.get("input[name='duenio']").type(NOMBRE_DUENIO);
+        cy.get("input[name='fecha']").type(FECHA_TURNO);
+        cy.get("input[name='hora']").type(HORA_RESERVA_TURNO);
+        cy.get("textarea[name='sintomas']").type(SINTOMAS_MASCOTA);
         cy.get("button[type='submit']").click();
+        cy.contains(NOMBRE_MASCOTA);
     });
 
     it("Deberia borrar la cita", () => {
